c76b76fa4ab8761fcd8739727c6b87b435d2f1b3
SAWEENX <139122122+Saveenx@users.noreply.github.com>
2025-05-18 18:30:18 +0530

    Add files via upload

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,9 +1,17 @@
 import random
 computer_choice = random.randint(0, 101)
 
-user_choice = int(input(' Enter a number between 0-100 :'))
-
-if not 0 <= user_choice <= 100:
-    print("Your number does not fulfill the requirments ")
+while True :
+ try:
+    user_choice = int(input('Enter a number between 0-100: '))
+    if user_choice < computer_choice :
+     print("Youre too low !")
+    elif user_choice > computer_choice:
+     print("Your too high !")
+    else:
+     print("congrats...You win !")
+     break
+ except ValueError:
+   print("Invalid input! Please enter a valid number.")
 
     
